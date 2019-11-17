bae8c8476ed8290683e50f48bc0f1e1da391b673
Jonathan Dyason <jonathandyason@hotmail.com>
2019-11-17 16:35:33 +0200

    new

--- main.lua
+++ main.lua
@@ -6,16 +6,32 @@ WINDOW_HEIGHT = 720
 -- -- background image and starting scroll location (X axis)
 local secretary = love.graphics.newImage('secretary.png')
 local teacher = love.graphics.newImage('teacher.png')
-local fairy = love.graphics.newImage('fairy.png')
-local a = love.graphics.newImage('A.png')
-local b = love.graphics.newImage('B.png')
-local c = love.graphics.newImage('C.png')
+local fairy = love.graphics.newImage('fairy-small.png')
+local cat = love.graphics.newImage('cat.png')
+local fairysetplay001 = love.graphics.newImage('fairy-setplay001.png')
+local fairysetplay002 = love.graphics.newImage('fairy-setplay002.png')
+
+
+-- local a = love.graphics.newImage('A.png')
+-- local b = love.graphics.newImage('B.png')
+-- local c = love.graphics.newImage('C.png')
 local isASelected = false
 local isBSelected = false
+
+local isCSelected = false
+
 local ablockX = 100
 local ablockY = 100
 local bblockX = 300
 local bblockY = 300
+
+local cblockX = 0
+local cblockY = 0
+
+local target1 = {x = 160, y = 430}
+
+
+
 local collision = false
 
 mouse = {}
@@ -32,8 +48,14 @@ function love.load()
     largeFont = love.graphics.newFont('font.ttf', 32)
     gameState = 'start'
 
+    -- for selecting
     aBlock = 	{hover = false, text = "A", x = 200, y = 100, call = selectBlockA}
     bBlock = 	{hover = false, text = "B", x = 200, y = 100, call = selectBlockB}
+
+    cBlock = 	{hover = false, text = "C", x = 0, y = 0, call = selectBlockC}
+
+    fairysprite = fairysetplay001
+
 end
 
 
@@ -75,7 +97,7 @@ function love.draw()
 
         -- draw stage
         love.graphics.rectangle('line', 20, 20, 900, 670)
-        love.graphics.draw(fairy, 900, 300, 0, 0.3, 0.3)
+        love.graphics.draw(fairy, 930, 150, 0, 1, 1)
         love.graphics.printf('Press "p" for', -50, 30, WINDOW_WIDTH, 'right')
         love.graphics.printf('setplay mode', -50, 70, WINDOW_WIDTH, 'right')
 
@@ -140,13 +162,72 @@ function love.draw()
         end
 
     elseif gameState == 'setplay' then
-        love.graphics.printf("test", 50, 50, 200, "center")
+
+       
 
          -- draw stage
-         love.graphics.rectangle('line', 20, 20, 900, 670)
-         love.graphics.draw(fairy, 900, 300, 0, 0.3, 0.3)
-      
+         love.graphics.rectangle('line', 00, 00, 950, 700)
+         love.graphics.rectangle('line', 00, 00, 600, 600)
+         love.graphics.rectangle('line', 80, 80, 440, 440)
+         love.graphics.rectangle('line', 600, 00, 350, 80)
+         love.graphics.rectangle('line', 600, 80, 350, 520)
 
+
+         love.graphics.draw(fairysprite, 600, 80, 0, 1, 1)
+
+
+        -- draw blocks
+        love.graphics.rectangle('line', cblockX, cblockY, 80, 80)
+        love.graphics.printf("C", cblockX - 8, cblockY + 24, 100, "center")
+
+        -- love.graphics.rectangle('line', 80, 0, 80, 80)
+        -- love.graphics.printf("A", 80 - 8, 40 - 16, 100, "center")
+
+        -- love.graphics.rectangle('line', 160, 0, 80, 80)
+        -- love.graphics.printf("T", 160 - 8, 40 - 16, 100, "center")
+
+
+        -- C block movement
+        if isCSelected == true then    
+            if collision == false then             
+                if love.keyboard.isDown('w') then
+                    cblockY = cblockY - 1
+                elseif love.keyboard.isDown('s') then
+                    cblockY = cblockY + 1
+                elseif love.keyboard.isDown('a') then
+                    cblockX = cblockX - 1
+                elseif love.keyboard.isDown('d') then
+                    cblockX = cblockX + 1
+                end
+            elseif collision == true then
+                if love.keyboard.isDown('w') then
+                    cblockY = cblockY + 10
+                elseif love.keyboard.isDown('s') then
+                    cblockY = cblockY - 10
+                elseif love.keyboard.isDown('a') then
+                    cblockX = cblockX + 10
+                elseif love.keyboard.isDown('d') then
+                    cblockX = cblockX - 10
+                end
+            end
+        end
+
+
+
+
+        -- draw subject
+        love.graphics.draw(cat, 200, 100, 0, 1, 1)
+
+      
+        love.graphics.rectangle('line', target1.x, target1.y, 80, 80)
+        love.graphics.rectangle('line', 260, 430, 80, 80)
+        love.graphics.rectangle('line', 360, 430, 80, 80)
+
+    end
+
+    if (cblockX == target1.x and cblockY == target1.y) then
+        love.graphics.printf("match", 100, 100, 100, "center")
+        fairysprite = fairysetplay002
     end
 
 end
@@ -159,8 +240,13 @@ function love.update(dt)
 	local	my = love.mouse.getY()
     menu_mousehandling(mx, my, down)   
 
-    collision = CheckCollision(ablockX, ablockY, bblockX, bblockY)
+    collision = CheckCollision(ablockX, ablockY, bblockX, bblockY, cblockX, cblockY)
     print(collision)
+
+
+  
+
+
 end
 
 
@@ -185,24 +271,36 @@ function menu_mousehandling(mx, my, down)
     end 
 
 
+    -- C block
+    if mx > cblockX and mx < cblockX + 80 and my > cblockY and my < cblockY + 80 then
+    	cBlock.hover=true
+    	if down == true then cBlock.call() end
+    else
+        cBlock.hover=false
+        if down == true then isCSelected = false end
+    end 
+
+
 end
 
 function selectBlockA()
-  
     if isASelected == false then
         isASelected = true      
-    end
-   
+    end   
 end
 
-
-
 function selectBlockB()
-  
     if isBSelected == false then
         isBSelected = true      
-    end
+    end   
+    
+end
+
+function selectBlockC()
+  
+    isCSelected = true      
    
+  
 end
 
 
